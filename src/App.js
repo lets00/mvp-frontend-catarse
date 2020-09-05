39ac25e2abbe7b03143e50885c66a4fccdac2b73
Luís Eduardo <eduardovansilva@gmail.com>
2020-09-05 15:02:02 -0300

    Add Header and Routes info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [login, setLogin] = useState({ user_id: '', jwt: '' })
+
+  const userLogged = user_login => {
+    setLogin(user_login);
+  }
+
+  const logout = () => {
+    setLogin({ user_id: '', jwt: '' })
+  }
+
   return (
     <div>
+      <Router>
+        <Header isAuth={true} logout={logout} />
+        <Switch>
+          <Route path="/" exact>
+          </Route>
+          <Route path="/login" exact>
+          </Route>
+          <Route path="/project/:id" exact>
+          </Route>
+          <Route path="/project-edit/:id" exact>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from '../images/logo_big.png'
+
+import { faEye } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
+
+export default function Header(props) {
+    return (
+        <>
+            <nav className="navbar align-center" style={{ "backgroundColor": "#503a80" }}>
+                <div className="d-flex justify-content-between">
+                    <Link to="/" className="navbar-brand mr-4">
+                        <img src={logo} alt="Logo catarse" height="30" className="d-inline-block align-top"></img>
+                    </Link>
+
+                    <a className="ml-4 mr-4 mt-2" href="/">Comece seu projeto</a>
+                    <form className="form-inline">
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Título do projeto" />
+                            <div className="input-group-append">
+                                <button type="button" className="btn btn-outline-secondary"> <FontAwesomeIcon icon={faEye} /></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                {props.isAuth ? <button className="btn btn-danger" onClick={props.logout}>Logout</button> : <Link className="btn btn-primary" to='/login'>Login</Link> }                
+            </nav>
+        </>
+    )
+}
